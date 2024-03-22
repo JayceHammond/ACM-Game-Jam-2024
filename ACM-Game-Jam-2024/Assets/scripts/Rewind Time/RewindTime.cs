3896f65c3a848f91c39a0b610aba76d30f0cddb8
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-03-22 16:38:57 -0500

    Rewind is working with stack

--- ACM-Game-Jam-2024/Assets/scripts/Rewind Time/RewindTime.cs
+++ ACM-Game-Jam-2024/Assets/scripts/Rewind Time/RewindTime.cs
@@ -10,36 +10,51 @@ public class RewindTime : MonoBehaviour
         public GameObject OldObject;
     }
 
-    private List<RewindData> RewindableObjectsData;
-    private List<GameObject> RewindableObjects;
+    //public List<RewindData> RewindableObjectsData;
+    public List<GameObject> RewindableObjects;
+
+    //Stack of Anchors
+    public Stack TimeStack;
 
     void Start(){
+        TimeStack = new Stack();
         assignRewindObjects();
     }
 
     private void assignRewindObjects(){
-        Debug.Log( GameObject.FindGameObjectsWithTag("Rewindable")[0]);
-        foreach(GameObject gameObject in GameObject.FindGameObjectsWithTag("Rewindable")){
-            RewindableObjects.Add(this.gameObject);
-            
+        //Debug.Log( GameObject.FindGameObjectsWithTag("Rewindable")[0]);
+        foreach(GameObject gameitem in GameObject.FindGameObjectsWithTag("Rewindable")){
+            RewindableObjects.Add(gameitem);
+            //Debug.Log(RewindableObjects[0]);
         }
     }
 
     public void RewindAnchor(){
+        List<RewindData> RewindableObjectsData = new List<RewindData>();
+
         foreach(GameObject item in RewindableObjects){
+
             RewindData savedItem = new RewindData
             {
                 Position = item.transform.position,
                 OldObject = item,
             };
-
+            Debug.Log(RewindableObjectsData);
             RewindableObjectsData.Add(savedItem);
         }
 
-
+        if(TimeStack.Count < 3){
+            TimeStack.Push(RewindableObjectsData);
+        }else{
+            Debug.Log("Too Many Anchors");
+            TimeStack.Pop();
+            TimeStack.Push(RewindableObjectsData);
+        }
+        
     }
 
     public void Rewind(){
+        List<RewindData> RewindableObjectsData = (List<RewindData>)TimeStack.Pop();
         foreach(RewindData item in RewindableObjectsData){
             foreach(GameObject gameObject in RewindableObjects){
                 if(item.OldObject == gameObject){
